95a8c61fd9bb3bb150f7d58202e363270cb0c6b8
Mark Abrahams <mark001616@gmail.com>
2022-11-04 12:39:32 -0500

    moved kat's homepage to its file and fixed oopsies in App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,10 @@ import "./App.css";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 //  file imports
-import TopBar from "./Components/TopBar";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import ProductReviewPage from "./pages/ProductReviewPage";
 import SearchPage from "./pages/SearchPage";
-import Layout from "./pages/Layout";
-import { Typography, Container, Grid, Box } from "@mui/material";
-import SearchBar from "./Components/SearchBar";
-import Tags from "./Components/Tags";
-
 
 function App() {
   //  react routers -> ui.dev/react-router-tutorial
@@ -27,27 +21,9 @@ function App() {
         {/* <Route path="*" element={<HomePage />} /> */}
       </Routes>
     </BrowserRouter>
-    <Grid sx={{background: 'linear-gradient(#47516F, #000000)'}}>
-      {/* Navigation bar on top */}
-      <TopBar />
-      <Container>
-      <Box component= "center" sx={{ height: "100vh", mt:25 }}>
-         <Typography variant="h3" align="center" color="white" fontWeight="bold">
-         Discover alternatives to your favorite softwares.
-          </Typography>
-          <Typography variant="h6" align="center" color="white" fontWeight= "medium" sx={{mt: 8, mb: 5}}>
-            Search for software or filter through tags.
-            </Typography>
-          <SearchBar/>
-          <Typography variant="h6" color="white" sx={{margin:2}}>Trending Tags</Typography>
-          <Tags/>
-      </Box>
-      </Container>
-    </Grid>
 
     // to test search page rn:
-    // <SearchPage />
-
+    // type /search on url bar :)
   );
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,42 @@
 import React from "react";
 
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, Grid } from "@mui/material";
 import { Container } from "@mui/system";
 import TopBar from "../Components/TopBar";
-import Layout from "./Layout";
+import Tags from "../Components/Tags";
+import SearchBar from "../Components/SearchBar";
 
 export default function HomePage() {
   return (
-    <Box>
+    <Grid sx={{ background: "linear-gradient(#47516F, #000000)" }}>
+      {/* Navigation bar on top */}
       <TopBar />
       <Container>
-        <Typography> Home Page</Typography>
+        <Box component="center" sx={{ height: "100vh", mt: 25 }}>
+          <Typography
+            variant="h3"
+            align="center"
+            color="white"
+            fontWeight="bold"
+          >
+            Discover alternatives to your favorite softwares.
+          </Typography>
+          <Typography
+            variant="h6"
+            align="center"
+            color="white"
+            fontWeight="medium"
+            sx={{ mt: 8, mb: 5 }}
+          >
+            Search for software or filter through tags.
+          </Typography>
+          <SearchBar />
+          <Typography variant="h6" color="white" sx={{ margin: 2 }}>
+            Trending Tags
+          </Typography>
+          <Tags />
+        </Box>
       </Container>
-    </Box>
+    </Grid>
   );
 }
