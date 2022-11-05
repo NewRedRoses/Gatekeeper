94dc3ac045e42cb4ffdb4b906fc5c652d3b08ae6
Mark <mark001616@gmail.com>
2022-11-04 19:23:11 -0500

    styling to product page  + topBar now returns to homePage

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Toolbar, Typography, AppBar } from "@mui/material";
+import { Outlet, Link } from "react-router-dom";
 
 export default function TopBar() {
   const purple = "#806491";
@@ -7,7 +8,9 @@ export default function TopBar() {
     <Box>
       <AppBar position="static">
         <Toolbar sx={{ backgroundColor: "#806491" }}>
-          <Typography> Gatekeeper</Typography>
+          <Link to="/">
+            <Typography color="white"> Gatekeeper</Typography>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,4 +1,13 @@
-import { Container, Typography, Button, Stack, Grid } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Button,
+  Grid,
+  Rating,
+  Stack,
+  Card,
+  Box,
+} from "@mui/material";
 
 import React from "react";
 import ProductCard from "../Components/ProductCard";
@@ -14,36 +23,74 @@ export default function ProductPage() {
   const productLogo =
     "https://upload.wikimedia.org/wikipedia/en/d/d7/Audacity_Logo_2-2-0.png";
 
-  return (
-    <div>
-      <TopBar />
-      <Container>
-        <Grid container spacing={3}>
-          <Grid item xs={4}>
-            <img src={productLogo} alt={productName + "'s Logo"} />
-          </Grid>
+  const [value, setValue] = React.useState(4);
 
-          <Stack spacing={3}>
+  const productLogoStyles = {
+    width: 400,
+    height: 300,
+    minWidth: 300,
+    minHeight: 300,
+  };
+  return (
+    <Box sx={{ backgroundColor: "#F6F4F1" }}>
+      <div>
+        <TopBar />
+      </div>
+
+      <Container sx={{ paddingTop: 5 }}>
+        <Grid container spacing={1}>
+          <Grid item xs={4}>
             <div>
               <Typography variant="h4"> {productName}</Typography>
             </div>
             <div>
-              <Typography variant="p"> {productDescription}</Typography>
+              <img
+                width={"100%"}
+                height={"100%"}
+                src={productLogo}
+                alt={productName + "'s Logo"}
+              />
             </div>
-            <div>
-              <a href={productLink}> {"-> " + productLink}</a>
-            </div>
-            <div>
-              <Button href="/review" variant="contained" color="secondary">
-                Write a Review
-              </Button>
-            </div>
-          </Stack>
-        </Grid>
-      </Container>
+          </Grid>
 
-      <h1>Thoughts of the Community</h1>
-      <UserReviewCard />
-    </div>
+          <Grid item xs>
+            <Stack spacing={2}>
+              <div>
+                <Typography variant="p"> {productDescription}</Typography>
+              </div>
+              <div>
+                <a href={productLink}> {"-> " + productLink}</a>
+              </div>
+              <div>
+                <Button href="/review" variant="contained" color="secondary">
+                  Write a Review
+                </Button>
+              </div>
+            </Stack>
+          </Grid>
+          <Grid item xs={3}>
+            <Card sx={{ paddingTop: 1, paddingLeft: 2 }}>
+              <Typography variant="h5"> Usability</Typography>
+              <div>
+                <Rating name="read-only" value={value} readOnly />
+              </div>
+              <Typography variant="h5"> Apperance</Typography>
+              <div>
+                <Rating name="read-only" value={3} readOnly />
+              </div>
+              <Typography variant="h5"> Customization</Typography>
+              <div>
+                <Rating name="read-only" value={2} readOnly />
+              </div>
+            </Card>
+          </Grid>
+        </Grid>
+
+        <div>
+          <h1>Thoughts of the Community</h1>
+          <UserReviewCard />
+        </div>
+      </Container>
+    </Box>
   );
 }
