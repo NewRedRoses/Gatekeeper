f5286bd56ee71a391b785683819c8d22faee378d
Mark <mark001616@gmail.com>
2022-11-06 12:09:16 -0600

    Initializing review page branch

--- src/pages/ProductReviewPage.js
+++ src/pages/ProductReviewPage.js
@@ -1,9 +1,25 @@
 import React from "react";
+import {
+  Container,
+  Typography,
+  Button,
+  Grid,
+  Rating,
+  Stack,
+  Card,
+  Box,
+} from "@mui/material";
+
+import TopBar from "../Components/TopBar";
 
 export default function ProductReviewPage() {
   return (
-    <div>
-      <p> Testing</p>
-    </div>
+    <Grid sx={{ backgroundColor: "#F6F4F1" }}>
+      <TopBar position="static" />
+
+      <Container>
+        <Typography> test</Typography>
+      </Container>
+    </Grid>
   );
 }
