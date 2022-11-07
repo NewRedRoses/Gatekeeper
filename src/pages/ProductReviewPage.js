90c5a79763b0197d70ad0f406278cc98c0aaced5
Kat Perez <kpere15@lsu.edu>
2022-11-06 22:10:56 -0600

    added review box + reccomendation buttons to review page

--- src/pages/ProductReviewPage.js
+++ src/pages/ProductReviewPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Grid, Stack, Typography, Rating, Chip } from "@mui/material";
+import { Box, Grid, Stack, Typography, Rating, Chip, TextField, Button } from "@mui/material";
 import TopBar from "../Components/TopBar";
 import SelectTag from "../Components/SelectTag"
 import { Container } from "@mui/system";
@@ -34,8 +34,8 @@ export default function ProductReviewPage() {
             </Grid>
 
             <Grid item xs>
-              <Stack direction="column">
-                <Typography variant="h3" fontWeight="bold">
+              <Stack direction="column" sx={{width:400, mt:1}}>
+                <Typography variant="h5" fontWeight="bold">
                   Rate {productName}'s Functionality
                 </Typography>
 
@@ -72,7 +72,7 @@ export default function ProductReviewPage() {
               </Stack>
             </Grid>
             <Grid item xs>
-              <Box component="center" sx={{ width: 300, height: 300, backgroundColor: "white", padding:2, borderRadius:5 }}>
+              <Box component="center" sx={{ width: 300, height: 250, backgroundColor: "white", padding:2, borderRadius:5 }}>
                 <Typography sx={{ textAlign: "center"}}> Select tags that describe the product from the drop-down menu.
                 <SelectTag/></Typography>
                 {/*chip array, probably what we want to use for the tags: https://mui.com/material-ui/react-chip/#chip-array*/}
@@ -85,6 +85,26 @@ export default function ProductReviewPage() {
                 </Stack>
               </Box>
             </Grid>
+            <Box sx={{ml:30, mt:5}}>
+              <TextField
+                variant="filled"
+                fullWidth={true}
+                multiline
+                rows={8}
+                label="Leave your thoughts!"
+                sx={{width:"600px",  borderRadius:5}}/>
+
+                <Typography variant="h5" fontWeight="bold" align="center" mt={2}>
+                Would you reccomed {productName} to a friend?
+                </Typography>
+
+                <Stack spacing={3} direction="row" sx={{ justifyContent:"center", mt:2}}>
+                    <Button variant="text"  sx={{color:"white", fontWeight:"bold", backgroundColor:"#4b8658"}}>Yes</Button>
+                    <Button variant="" sx={{color:"white",fontWeight:"bold", backgroundColor:"#FFD800"}}>Maybe</Button>
+                    <Button variant="" sx={{color:"white", fontWeight:"bold", backgroundColor:"#FF5D00"}}>No</Button>
+                </Stack>
+                <Button variant="text" sx={{color:"white", textTransform:"capitalize", fontWeight:"bold", backgroundColor:"#806491", mt:2, ml: 30}}>Post Your Review!</Button>
+            </Box>
           </Grid>
         </Container>
       </Grid>
