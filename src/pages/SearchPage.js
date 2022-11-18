ba575cc7298a1500c1737dcaef777d9678cb12a8
Mark <mark001616@gmail.com>
2022-11-17 21:41:36 -0600

    Basic layout for search page

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,16 +1,54 @@
 import React from "react";
 import Header from "../Components/header";
 import Footer from "../Components/footer";
+import { Grid, Box, Container } from "@mui/material";
+// File imports
+import SearchBar from "../Components/SearchBar";
 import ProductCard from "../Components/ProductCard";
 
+const sampleData = [
+  "gimp",
+  "firefox",
+  "audacity",
+  "gimp",
+  "firefox",
+  "audacity",
+  "gimp",
+  "firefox",
+  "audacity",
+  "gimp",
+  "firefox",
+  "audacity",
+  "gimp",
+  "firefox",
+  "audacity",
+  "gimp",
+  "firefox",
+  "audacity",
+];
 
 export default function SearchPage() {
   return (
-    <>
-            <Header />
-            <ProductCard name="gimp" />
-            <Footer />
-    </>
+    <Box>
+      <Header />
+      <Grid>
+        <Box width="100%" display="flex" justifyContent="center">
+          <SearchBar />
+        </Box>
+        <Container sx={{ paddingBottom: 5 }}>
+          <Grid container spacing={4}>
+            {sampleData.map((name) => {
+              return (
+                <Grid item xs={0}>
+                  <ProductCard name={name} />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Container>
+
+        <Footer />
+      </Grid>
+    </Box>
   );
 }
-
